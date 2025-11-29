85b22bb83801452c2e37988b409cc3e92e645292
Joys <114014394+JoysJ@users.noreply.github.com>
2025-11-29 18:34:52 -0500

    changed model

--- model_setup.py
+++ model_setup.py
@@ -3,69 +3,31 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import models
 
-class RES_MODEL(nn.Module):
-    def __init__(self, in_d, h_d, num_classes):
-        super().__init__()
-        self.l1 = nn.Linear(in_d, h_d)
-
-
-        self.r1 = nn.ReLU()
-     
-
-        self.l2 = nn.Linear(h_d, h_d//2)
-
-
-        self.r2 = nn.ReLU()
-
-
-
-        self.l3 = nn.Linear(h_d//2, num_classes)
-    def forward(self, x):
-        x = self.l1(x)
-
-        x = self.r1(x)
-
-
-
-        x = self.l2(x)
-
-        x = self.r2(x)
-
-
-
-        x= self.l3(x)
-        return x
-
-
-
-
 def build_model(num_classes, gray_scale, freeze_backbone, lr):
-    my_model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+    # Load Pre-trained ResNet
+    model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
 
-    if(gray_scale==True):
-        old_weight = my_model.conv1.weight.data
-        new_conv = nn.Conv2d(
-        1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+    if gray_scale:
+        old_weight = model.conv1.weight.data
+        new_conv = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
         new_conv.weight.data = old_weight.mean(dim=1, keepdim=True)
-        my_model.conv1 = new_conv
+        model.conv1 = new_conv
 
-
-    features = my_model.fc.in_features
-    my_model.fc = RES_MODEL(features, 256, num_classes)
+    # .pth file expects a single Linear layer here
+    features = model.fc.in_features
+    model.fc = nn.Linear(features, num_classes)
 
     if freeze_backbone:
-        for name, param in my_model.named_parameters():
+        for name, param in model.named_parameters():
             if "fc" not in name:
                 param.requires_grad = False
 
-    train_params = []
-    for param in my_model.parameters():
-        if param.requires_grad == True:
-            train_params.append(param)
-
+    if freeze_backbone:
+        train_params = model.fc.parameters()
+    else:
+        train_params = model.parameters()
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(train_params, lr=lr)
 
-
-    return my_model, criterion, optimizer
+    return model, criterion, optimizer
